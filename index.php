29ce65095e66d24107c546b3d74d96fa94fc69e4
Luter <lutersergei@mail.ru>
2016-05-22 13:59:10 +0700

    add photo.php & images

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: drKox
- * Date: 22.05.2016
- * Time: 13:23
- */
+error_reporting(E_ALL);
+?>
+    <!DOCTYPE html>
+    <html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Lesson_3</title>
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+    <div class="container">
+        <div class="col-md-12">
+            <h1>Урок 4. Запросы HTTP, параметры URL и формы HTML</h1>
+<?php
+?>
+            </div>
+        </div>
+    </body>
+
---
+++ photo.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: drKox
+ * Date: 22.05.2016
+ * Time: 13:52
+ */
