ac9d357178a010aafa9ae5b9cca1347dbe0c4843
Luter <lutersergei@mail.ru>
2016-05-22 14:56:46 +0700

    add gallery

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php
 error_reporting(E_ALL);
 ?>
-    <!DOCTYPE html>
-    <html lang="ru">
-    <head>
-        <meta charset="UTF-8">
-        <title>Lesson_3</title>
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    </head>
-    <body>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Lesson_4</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
     <div class="container">
         <div class="col-md-12">
             <h1>Урок 4. Запросы HTTP, параметры URL и формы HTML</h1>
-<?php
-?>
-            </div>
+            <h2>Галерея</h2>
+            <?php
+            for($i = 1; $i <= 4; $i++)
+            {
+                echo "<a href = photo.php?id=$i target=\"_blank\">Фото №$i</a><br/>";
+            }
+            ?>
         </div>
-    </body>
+    </div>
+</body>
 
--- photo.php
+++ photo.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: drKox
- * Date: 22.05.2016
- * Time: 13:52
- */
+error_reporting(E_ALL);
+$id = $_GET['id']; // Считываем передаваемый параметр
+$image_description=[1 => "Мост днем", 2 => "Мост ночью",3 => "Общий вид днем",4 => "Общий вид ночью"]
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta charset="UTF-8">
+    <title>Просмотр фото № <?php echo $id;?></title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="col-md-12">
+            <a href="img/<?php echo $id;?>.jpg" target="_blank"><img src="img/<?php echo $id;?>.jpg" alt="photo_<?php echo $id;?>" width="600px"></a>
+            <h2><?php echo $image_description[$id] ?></h2>
+        </div>
+    </div>
+</body>
+</html>
